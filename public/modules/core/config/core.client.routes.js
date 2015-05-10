1ce683a03244f918797494bd114e27cfd6b88687
Alex Newman <alex.newman@wandisco.com>
2015-05-10 00:23:15 -0700

    f0rk!

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -12,5 +12,13 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		});
+
+		// Home state routing
+		$stateProvider.
+		state('compass', {
+			url: '/compass',
+			templateUrl: 'modules/core/views/compass.html'
+		});
+
 	}
 ]);
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,49 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$timeout',
+	function($scope, Authentication, $timeout) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.test = "fuck!"
+
+
+		var promise = new FULLTILT.getDeviceOrientation({ 'type': 'world' });
+
+	 	var deviceOrientation;
+
+		$scope.fortune = "you will have a wonderful day and someone will share a cigratte with you."
+
+	  promise.then(function(controller) {
+	      // Store the returned FULLTILT.DeviceOrientation object
+	      deviceOrientation = controller;
+				draw()
+
+	    })
+	    .catch(function(message) {
+	      console.error(message);
+
+	      // Optionally set up fallback controls...
+	      // initManualControls();
+	    });
+
+			function draw() {
+					if (deviceOrientation) {
+
+						// Obtain the *screen-adjusted* normalized device rotation
+						// as Quaternion, Rotation Matrix and Euler Angles objects
+						// from our FULLTILT.DeviceOrientation object
+						$scope.quaternion = deviceOrientation.getScreenAdjustedQuaternion();
+						$scope.matrix = deviceOrientation.getScreenAdjustedMatrix();
+						$scope.euler = deviceOrientation.getScreenAdjustedEuler();
+
+						$timeout( function(){
+							draw()
+						}, 1000 )
+
+				}
+
+			}
+
 	}
 ]);
